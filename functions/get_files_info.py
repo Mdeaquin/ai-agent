c9ae221bea39bddc5b025b9abc8055424a19f0de
Mdeaquin <mdeaquin21@gmail.com>
2025-08-25 14:20:16 -0500

    fix: get file info and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,18 +4,25 @@ def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.normpath(os.path.abspath(working_directory))
     path_to_check = os.path.normpath(os.path.abspath(os.path.join(working_directory, directory)))
     if path_to_check.startswith(abs_working_dir) is False:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(path_to_check):
-        return f'Error: "{directory}" is not a directory'
+        return f'    Error: "{directory}" is not a directory'
     else:
-        file_list = os.listdir(path_to_check)
-        for file in file_list:
-            file_name = file
-            path_to_file = os.path.join(directory, file)
-            file_size = os.path.getsize(path_to_file)
-            if os.path.isfile(path_to_file) is True:
-                is_dir = False
-            else: 
-                is_dir = True
+        file_lines = []
+        file_list = sorted(os.listdir(path_to_check), key=lambda filename: (os.path.isdir(os.path.join(path_to_check, filename)), filename))
+        try:
+            for file in file_list:
+                file_name = file
+                path_to_file = os.path.join(path_to_check, file)
+                if os.path.isdir(path_to_file) is True:
+                    is_dir = True
+                    file_size = 92
+                else:
+                    is_dir = False
+                    file_size = os.path.getsize(path_to_file)
+                if not file_name.startswith("__"):
+                    file_lines.append(f" - {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
 
-            print(f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
+            return "\n".join(file_lines)
+        except Exception as e:
+            return f"    Error: An unexpected error occurred: {e}"
